44f7e31133b3f27710aee7ea6153b56ec5003dae
dchele <dchele@PCDS29>
2023-03-08 15:37:22 -0500

    Agregar archivos de proyecto.

---
+++ FileLogger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger;
+
+public class FileLogger : ILogger{
+    public string GetRutaApp()
+    {
+        string Ruta = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+        return Ruta;
+    }
+
+    public string GetRutaCarpetaMes()
+    {
+        string Ruta = GetRutaApp() + "\\" + DateTime.Now.Year.ToString() + "\\" + DateTime.Now.Month.ToString("00");
+        return Ruta;
+    }
+
+    public string GetRutaFileLog()
+    {
+        string Ruta = GetRutaCarpetaMes() + "\\" + DateTime.Today.ToString("ddMMyyyy") + " Log.log";
+        return Ruta;
+    }
+    public string GetRutaFileIni()
+    {
+        string Ruta = GetRutaApp() + "\\INI.xml";
+        return Ruta;
+    }
+    public void CreaArchivoLog()
+    {
+        Directory.CreateDirectory(GetRutaCarpetaMes());
+        FileStream archivoLog = File.Create(GetRutaFileLog());
+        archivoLog.Close();
+    }//Fin CreaArchivoLog
+
+    public void Log(string message){
+        if (!File.Exists(GetRutaFileLog())){
+            CreaArchivoLog();
+        }
+
+        string HoraEvento = DateTime.Now.ToString("HH:mm:ss");
+        using StreamWriter streamWriter = File.AppendText(GetRutaFileLog());
+        streamWriter.WriteLine(message + " " + HoraEvento);
+    }
+}
